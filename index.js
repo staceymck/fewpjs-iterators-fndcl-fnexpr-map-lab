fdb8c07db24b48321af27ca6c3df0676f5af6667
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-21 12:25:11 -0500

    commit tree 41dad4d646c8e5e7e62095ccee2d371a90a3bdf1

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(tutorial) {
+    let wordsArray = tutorial.split(' ');
+    let casedWords = wordsArray.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return casedWords.join(' ');
+  });
 }
